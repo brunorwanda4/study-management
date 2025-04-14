1de5577f19a478a53e0d01524b7282e5e1f5b941
brunorwanda4 <brunorwanda4@gmail.com>
2025-04-14 10:33:44 +0200

    add locale in application

---
+++ frontend/src/app/[lang]/auth/login/page.tsx
@@ -0,0 +1,33 @@
+import ThemeSwitcher from '@/components/theme/change-theme'
+import { Button } from '@/components/ui/button'
+import { getDictionary, Locale } from '@/i18n';
+import React from 'react'
+
+interface Props {
+  params: Promise<{ lang: Locale }>;
+  searchParams: Promise<{ oauthError?: string }>;
+}
+
+
+const LoginPage = async ({params, searchParams}: Props) => {
+  const lang = (await params).lang;
+  const oauthError = (await searchParams).oauthError
+  const dictionary = await getDictionary(lang);
+
+  return (
+    <div className=' h-screen'>
+        dictionary : {dictionary.auth.login.page.title}
+      <Button>Hello Rwanda</Button>
+      <ThemeSwitcher />
+      <button className=' btn btn-primary'>Hello Button</button>
+
+      <div>
+        error :{oauthError}
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
+
+
